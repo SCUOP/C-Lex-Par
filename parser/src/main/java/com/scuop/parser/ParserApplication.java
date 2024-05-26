6a92c7178f60d5134058ca51c4b3a50dc3799206
SCUOP <scuop.top@gmail.com>
2024-05-26 17:11:01 +0800

    打包

--- parser/src/main/java/com/scuop/parser/ParserApplication.java
+++ parser/src/main/java/com/scuop/parser/ParserApplication.java
@@ -11,11 +11,38 @@ public class ParserApplication {
 	public static void main(String[] args) throws IOException {
 		TreeNode res = null;
 		LexerService lexer = new LexerService();
-		lexer.setPath("/home/ubuntu/workspace/C-LexPar/test/input1.c", null);
+		// lexer.setPath("/home/ubuntu/workspace/C-Lex-Par/test/input1.c", null);
 		Parser parser = new Parser(lexer);
-		res = parser.parse();
-		if (res != null) {
-			res.show();
+		switch (args.length) {
+			case 0:
+				System.out.println("请输入文件路径参数");
+				break;
+			case 1:
+				try {
+					lexer.setPath(args[0], "");
+					res = parser.parse();
+					if (res != null) {
+						res.show();
+					}
+					break;
+				} catch (IOException e) {
+					System.out.println("文件路径有误");
+					e.printStackTrace();
+					break;
+				}
+			case 2:
+				try {
+					lexer.setPath(args[0], args[1]);
+					res = parser.parse();
+					if (res != null) {
+						res.show();
+					}
+					break;
+				} catch (IOException e) {
+					System.out.println("文件路径有误");
+					e.printStackTrace();
+					break;
+				}
 		}
 	}
 
